810ebca0a0f1f83dc692e6425e611183a05e68cd
Thitima <40798188+thitimas@users.noreply.github.com>
2022-07-06 17:12:48 -0400

    Add files via upload

---
+++ password_escape.js
@@ -0,0 +1,56 @@
+
+function buttonFunction() {
+    
+   
+  var password = document.getElementById("password").value;
+
+  var valid = false;
+  if (checkpassword(password) == false) {
+    $("#passwordFeedback").html("Incorrect username or password. Please try again.");
+    $("#passwordFeedback").css("color", "#dc3545");
+
+  } else {
+    $("#passwordFeedback").html("Got it! This is strong enough! <br/>Please remember it and use it for later! <br> Now go to <a href='https://mywordle.strivemath.com/?word=zsgws' target='_blank'>https://mywordle.strivemath.com/?word=zsgws</a> for the next clue!!");
+    $("#passwordFeedback").css("color", "#28a745");
+    localStorage.setItem('GenCyberPass', password);
+
+  }
+}
+
+var code = document.getElementById("password");
+
+// var strengthbar = document.getElementById("meter");
+// // var display = document.getElementsByClassName("textbox")[0];
+
+// code.addEventListener("keyup", function() {
+//   checkpassword(code.value);
+// });
+
+
+function checkpassword(password) {
+  var strength = 0;
+  if (password.match(/[a-z]+/)) {
+    strength += 1;
+  }
+  if (password.match(/[A-Z]+/)) {
+    strength += 1;
+  }
+  if (password.match(/[0-9]+/)) {
+    strength += 1;
+  }
+  if (password.match(/[$@#&!]+/)) {
+    strength += 1;
+  }
+  if (password.length >= 8){
+    strength += 1;    
+  }
+
+  console.log(strength);
+  if (strength > 3){
+    return true;
+  } else {
+    return false;
+  }
+}
+
+
